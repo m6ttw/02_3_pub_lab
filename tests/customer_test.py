939b01edca61684e6e28912594565d67128856ff
m6ttw <matthewrhyswilliams@live.com>
2020-11-25 10:45:58 +0000

    initial commit

---
+++ tests/customer_test.py
@@ -0,0 +1,10 @@
+# A Customer should be able to buy a Drink from the Pub,
+# reducing the money in its wallet
+# and increasing the money in the Pub's till
+
+import unittest
+from src.customer import Customer
+from src.pub import Pub
+from src.drink import Drink
+
+    
